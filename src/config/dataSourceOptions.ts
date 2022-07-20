182b52d21db9fe46303fe51c3e7c10582639509b
Guilherme Feitosa <guifcm12@gmail.com>
2022-07-20 10:34:03 -0300

    Add root path on data source options

--- src/config/dataSourceOptions.ts
+++ src/config/dataSourceOptions.ts
@@ -1,16 +1,18 @@
 import { database_url, env } from "./enviroment";
 import { DataSourceOptions } from "typeorm";
 
+const root = env == 'development' ? 'src' : 'dist'
 const extention = env == 'development' ? '.ts' : '.js'
+
+console.log(root + extention)
 
 const dataSourceOptions: DataSourceOptions = {
     type: "postgres",
     url: database_url,
     logging: false,
-    entities: ['src/models/*' + extention],
-    migrations: ['src/database/migrations/*' + extention],
-    subscribers: [],
-    migrationsRun: true
+    entities: [root + '/models/*' + extention],
+    migrations: [root + '/database/migrations/*' + extention],
+    subscribers: []
 }
 
 export default dataSourceOptions
