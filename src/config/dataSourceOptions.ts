5c0692d42cc1a175b078de18b1b749a2ad70eb16
Guilherme Feitosa <guifcm12@gmail.com>
2022-07-20 22:37:12 -0300

    Add ssl configuration on data source options

--- src/config/dataSourceOptions.ts
+++ src/config/dataSourceOptions.ts
@@ -4,15 +4,16 @@ import { DataSourceOptions } from "typeorm";
 const root = env == 'development' ? 'src' : 'dist'
 const extention = env == 'development' ? '.ts' : '.js'
 
-console.log(root + extention)
-
 const dataSourceOptions: DataSourceOptions = {
     type: "postgres",
     url: database_url,
     logging: false,
     entities: [root + '/models/*' + extention],
     migrations: [root + '/database/migrations/*' + extention],
-    subscribers: []
+    subscribers: [],
+    ssl: {
+        rejectUnauthorized: true
+    }
 }
 
 export default dataSourceOptions
