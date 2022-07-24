bb55f6c2e1ae468a4e32302a8ff982ea39a904de
Guilherme Feitosa <guifcm12@gmail.com>
2022-07-24 11:05:37 -0300

    add note and status changes on websocket

--- src/websocket.ts
+++ src/websocket.ts
@@ -15,25 +15,39 @@ interface OnlineUser {
    firstName: string
    lastName: string
    status?: string
-   message?: string
+   note?: string
 }
 const users: OnlineUser[] = [] 
 
 io.on('connection', socket => {
    socket.on('login', data => {
       socket.join('contact-list')
-      let { username, firstName, lastName, status, message } = data
+      let { username, firstName, lastName, status, note } = data
       
       const user: OnlineUser = {
          socket_id: socket.id,
-         username, firstName, lastName, status, message
+         username, firstName, lastName, status, note
       }
 
-      const userRoom = users.find(user => user.username === data.username)
+      const userRoom = users.find(user => user.username === username)
       if (userRoom) userRoom.socket_id = socket.id
       else users.push(user)
 
       io.to('contact-list').emit('login', users)
+   })
+
+   socket.on('noteChange', data => {
+      const user = users.find(user => user.username === data.username)
+      user.note = data.note
+
+      io.to('contact-list').emit('noteChange', user)
+   })
+
+   socket.on('statusChange', data => {
+      const user = users.find(user => user.username === data.username)
+      user.status = data.status
+
+      io.to('contact-list').emit('statusChange', user) 
    })
 
    socket.on('logoff', data => {
@@ -44,7 +58,7 @@ io.on('connection', socket => {
    })
 
    socket.on('message', data => {
-      const { sender, recipient, text } = data
+      let { sender, recipient, text } = data
 
       const messageService = new MessageService()
       const message = messageService.create({ sender, recipient, text })
